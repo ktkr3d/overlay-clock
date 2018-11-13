f31490c732eb5cd352e95243f1968e7d3de714a2
ktkr3d <ktkr3d@gmail.com>
2018-11-13 21:30:51 +0900

    fix respawn after disabled

--- extension.js
+++ extension.js
@@ -3,40 +3,40 @@ const Main = imports.ui.main;
 const Mainloop = imports.mainloop;
 
 const REFRESH_INTERVAL = 10;
-let text;
-
-function _showClock() {
-    let clockDate = new Date();
-    let dateFormat = _("%H:%M");
-    if (!text) {
-        text = new St.Label({ style_class: 'overlayclock-label', text: "" });
-        Main.uiGroup.add_actor(text);
-    }
-    text.text = clockDate.toLocaleFormat(dateFormat);
-
-    let monitor = Main.layoutManager.primaryMonitor;
-
-    text.set_position(monitor.x + Math.floor(5),
-                      monitor.y + Math.floor(monitor.height - text.height));
-
-    set_timer();
-}
+let clockLabel;
 
 function init() {
-    _showClock();
 }
 
 function enable() {
-    _showClock();
+    if (!clockLabel) {
+        clockLabel = new St.Label({ style_class: 'overlayClockLabel', text: "00:00" });
+        Main.uiGroup.add_actor(clockLabel);
+    }
+
+    let monitor = Main.layoutManager.primaryMonitor;
+    clockLabel.set_position(monitor.x + Math.floor(11),
+                            monitor.y + Math.floor(monitor.height - clockLabel.height));
+
+    updateClock();
 }
 
 function disable() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
+    Main.uiGroup.remove_actor(clockLabel);
+    clockLabel = null;
+}
+
+function updateClock() {
+    if (clockLabel) {
+        let clockDate = new Date();
+        let dateFormat = _("%H:%M");
+        clockLabel.text = clockDate.toLocaleFormat(dateFormat);
+        set_timer();
+    }
 }
 
 function set_timer() {
     Mainloop.timeout_add_seconds(REFRESH_INTERVAL, function(){
-      _showClock();
+        updateClock();
     });
 }
